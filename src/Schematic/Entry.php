6b4ec05859790f96d239057c366e9f914cbab479
Vojtěch Kohout <v.kohout@gmail.com>
2016-08-08 15:09:40 +0200

    Simplified code little bit and improved support for NULL

--- src/Schematic/Entry.php
+++ src/Schematic/Entry.php
@@ -63,33 +63,19 @@ class Entry
 	public function __get($name)
 	{
 		if (!isset($this->associationTypes[$name]) || isset($this->initializedAssociations[$name])) {
-			if (!array_key_exists($name, $this->data)) {
-				throw new InvalidArgumentException("Missing field '$name'.");
-			}
-
-			return $this->data[$name];
+			return $this->readData($name);
 		}
 
 		$this->initializedAssociations[$name] = TRUE;
 
+		$data = isset($this->embeddedEntriesIndex[$name]) ? $this->readEmbeddedEntry($name) : $this->readData($name);
+
+		if ($data === NULL) {
+			return $this->data[$name] = NULL;
+		}
+
 		$associationType = $this->associationTypes[$name];
 		$entriesClass = $this->entriesClass;
-
-		if (!isset($this->embeddedEntriesIndex[$name])) {
-			if (!array_key_exists($name, $this->data)) {
-				throw new InvalidArgumentException("Missing field '$name'.");
-			}
-			$data = $this->data[$name];
-
-		} else {
-			$data = [];
-			foreach ($this->embeddedEntriesIndex[$name] as $field => $prefixedField) {
-				if (!array_key_exists($prefixedField, $this->data)) {
-					throw new InvalidArgumentException("Missing field '$prefixedField'.");
-				}
-				$data[$field] = $this->data[$prefixedField];
-			}
-		}
 
 		return $this->data[$name] = is_array($associationType) ?
 			new $entriesClass($data, reset($associationType)) :
@@ -118,6 +104,40 @@ class Entry
 				$this->embeddedEntriesIndex[$nameWithoutPeriod][$field] = $prefix . $field;
 			}
 		}
+	}
+
+
+	/**
+	 * @param string $name
+	 * @return array|NULL
+	 */
+	private function readEmbeddedEntry($name)
+	{
+		$values = [];
+		$isEmpty = TRUE;
+		foreach ($this->embeddedEntriesIndex[$name] as $field => $prefixedField) {
+			$values[$field] = $this->readData($prefixedField);
+
+			if ($values[$field] !== NULL) {
+				$isEmpty = FALSE;
+			}
+		}
+
+		return $isEmpty ? NULL : $values; // unfortunately this is still just estimation
+	}
+
+
+	/**
+	 * @param string $name
+	 * @return mixed
+	 */
+	private function readData($name)
+	{
+		if (!array_key_exists($name, $this->data)) {
+			throw new InvalidArgumentException("Missing field '$name'.");
+		}
+
+		return $this->data[$name];
 	}
 
 }
